5d32aed08d0efc79ba92c2e3cfbf02ee05bb1f04
Brandon Wharton <whartonbm@gmail.com>
2021-06-11 18:41:37 -0500

    Created FeelingFeedback component, tested import/export

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+// component imports
+import FeelingFeedback from '../FeelingFeedback/FeelingFeedback';
 
 function App() {
 
@@ -11,6 +13,7 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <FeelingFeedback />
     </div>
   );
 }
---
+++ src/components/FeelingFeedback/FeelingFeedback.jsx
@@ -0,0 +1,8 @@
+function FeelingFeedback () {
+
+    return (
+        <h2>In FeelingFeedback</h2>
+    )
+}
+
+export default FeelingFeedback;

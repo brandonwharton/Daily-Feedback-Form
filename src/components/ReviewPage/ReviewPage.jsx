eebad3267a1ff28c4dcb74857ca1bb6338681c0b
Brandon Wharton <whartonbm@gmail.com>
2021-06-12 11:57:44 -0500

    Brought in feedbackData reducer data into ReviewPage, data is rendering to DOM

--- src/components/ReviewPage/ReviewPage.jsx
+++ src/components/ReviewPage/ReviewPage.jsx
@@ -1,7 +1,18 @@
-function ReviewPage () {
+import { useSelector } from "react-redux";
 
+function ReviewPage () {
+    // get data from feedbackData reducer in redux store
+    const feedbackData = useSelector(store => store.feedbackData)
+
+    console.log(feedbackData);
     return (
-        <h2>In ReviewPage</h2>
+        <div>
+            <h2>Review Your Feedback</h2>
+            <h3>Feelings: {feedbackData.feeling}</h3>
+            <h3>Understanding: {feedbackData.understanding}</h3>
+            <h3>Supported: {feedbackData.support}</h3>
+            <h3>Comments: {feedbackData.comments}</h3>
+        </div>
     )
 }
 

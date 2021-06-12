7173d685da2ea35b7629d04a9fc3d0fe663d62fc
Brandon Wharton <whartonbm@gmail.com>
2021-06-12 13:58:31 -0500

    New feedback button is dispatching to feedbackData reducer to reset state for a new form and navigating back to the first page

--- src/components/SuccessPage/SuccessPage.jsx
+++ src/components/SuccessPage/SuccessPage.jsx
@@ -1,13 +1,27 @@
+// hooks
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
 // material-UI components
 import Button from '@material-ui/core/Button';
 
+
+
 function SuccessPage () {
+    // set up dispatch to reducers
+    const dispatch = useDispatch();
+    // useHistory to navigate to other routes
+    const history = useHistory()
 
     // click handler for the New Feedback button
     const startNewFeedback = (event) => {
         // keep page from refreshing on click
         event.preventDefault();
-        console.log('clicked!');
+        // dispatch action to feedbackData reducer to reset state for a new feedback form
+        dispatch({
+            type: 'RESET_FEEDBACK'
+        });
+        // navigate back to beginning of feedback form
+        history.push('/');
     }
 
 
